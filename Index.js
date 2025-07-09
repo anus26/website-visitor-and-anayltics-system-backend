f68ec055fff47b7f5233b7eb4a2db3debaaba831
anus26 <anusraza268gmail.com>
2025-07-08 19:25:29 -0700

    src setup

--- Index.js
+++ Index.js
@@ -1,9 +1,10 @@
 import dotenv from 'dotenv'
 dotenv.config()
 import cors from 'cors'
-
 import express  from 'express'
+import connectDB from './src/config'
 const app = express()
+connectDB
 
 
 app.get('/', (req, res) => {
---
+++ src/config/index.js
@@ -0,0 +1,17 @@
+import mongoose  from "mongoose";
+
+
+const connectDB=async()=>{
+    try {
+        const connectionInstance=await mongoose.connect(
+            `${process.env.MONGODB_URI}prative`
+        )
+        console.log(` Mongodb connect !! DB Host ${connectionInstance.connection.host}`);
+        
+    } catch (error) {
+        console.log('Mongodb connection failed',error);
+        process.exit(1)
+        
+    }
+}
+export default connectDB
