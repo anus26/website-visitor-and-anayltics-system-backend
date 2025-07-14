b201379839e7711c7200fe4a0ef64323a15700ac
anus26 <anusraza268gmail.com>
2025-07-13 18:50:39 -0700

    modified

--- api/visitor.js
+++ api/visitor.js
@@ -1,18 +1,24 @@
 // api/visitor.js
 import express from 'express'
+import visitorroutes from '../src/routes/visitorroutes.js'
 import cors from 'cors'
+import dotenv from 'dotenv'
 import useragent from 'express-useragent'
 import requestIp from 'request-ip'
 import connectDB from '../src/config/index.js'
-import visitorroutes from '../src/routes/visitorroutes.js'
-const app = express()
 
+dotenv.config()
+
+const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(requestIp.mw())
 app.use(useragent.express())
 
 connectDB()
+
 app.use('/api', visitorroutes)
 
+// Vercel-compatible handler export
 export default app
+
